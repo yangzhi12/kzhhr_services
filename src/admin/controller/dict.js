007594ec8a8bbb87a1fcf737935d2e38f3943a06
362479845 <362479845@qq.com>
2019-03-29 23:03:37 +0800

    增加了字典及合同的controller

---
+++ src/admin/controller/dict.js
@@ -0,0 +1,28 @@
+const Base = require('./base.js');
+
+module.exports = class extends Base {
+  async indexAction() {
+    const page = this.post('page') || 1;
+    const size = this.post('size') || 100;
+    const no = this.post('no') || '';
+    const name = this.post('name') || '';
+    const dictname = this.post('dictname') || '';
+    if (think.isEmpty(dictname)) {
+      return this.fail(401, '请指定字典名称.');
+    } else {
+      const dicts = ['voltage', 'transformer', 'plantype', 'planitem', 'plan'];
+      if (dicts.indexOf(dictname) !== -1) {
+        const model = this.model(dictname);
+        const condition = `(${dictname}no like '%${no}%') OR (${dictname}name like '%${name}%')`;
+        const data = await model
+          .where(condition)
+          .order(['id ASC'])
+          .page(page, size)
+          .countSelect();
+      } else {
+        return this.fail(401, '请检查字典名称是否有误.');
+      }
+    }
+    return this.success(data);
+  }
+};
