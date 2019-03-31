7d35f0d6cef62adfd4c3538cd3cae62de9db2975
362479845 <362479845@qq.com>
2019-03-31 21:03:09 +0800

    增加合同管理列表查询服务

--- src/admin/controller/contract.js
+++ src/admin/controller/contract.js
@@ -1,3 +1,46 @@
 const Base = require('./base.js');
 
-module.exports = class extends Base {};
+module.exports = class extends Base {
+  /**
+   * index action
+   * @return {Promise} []
+   */
+  async indexAction() {
+    const page = this.post('page') || 1;
+    const size = this.post('size') || 10;
+    const no = this.post('no') || '';
+    const name = this.post('name') || '';
+    const model = this.model('contract');
+    const data = await model
+      .field([
+        'con.id',
+        'con.contractno',
+        'con.contractname',
+        'con.contractvalue',
+        'con.contractstart',
+        'con.contractend',
+        'us.username',
+        'con.contractstate'
+      ])
+      .alias('con')
+      .join({
+        table: 'user',
+        join: 'left',
+        as: 'us',
+        on: ['us.id', 'con.userid']
+      })
+      .where(
+        "(con.contractno like '%" +
+          `${no}` +
+          "%')" +
+          "OR (con.contractname like '%" +
+          `${name}` +
+          "%')"
+      )
+      .order(['con.id DESC'])
+      .page(page, size)
+      .countSelect();
+
+    return this.success(data);
+  }
+};
